3f827f98989573617e9a593f9338f7895d11ea6f
Eugene Nunoo <khemikal2016@gmail.com>
2023-05-31 21:16:44 +0100

    create task

--- src/app/service/crud.service.ts
+++ src/app/service/crud.service.ts
@@ -1,30 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Task } from '../model/task';
 import { Observable } from 'rxjs';
+import { Task } from '../model/task';
+
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
 
-  serviceUrl :string ;
-  constructor(private http:HttpClient) { 
-    this.serviceUrl = 'http://localhost:3000/tasks'
-   }
+  serviceURL : string ;
 
-   addTask(task:Task):Observable<Task>{
-    return this.http.post<Task>(this.serviceUrl,task);
-   }
+  constructor(private http : HttpClient) {
+    this.serviceURL = "http://localhost:3000/task"
+  }
 
-  getAllTasks():Observable<Task[]>{
-    return this.http.get<Task[]>(this.serviceUrl);
-   }
+  addTask(task : Task) : Observable<Task> {
+    return this.http.post<Task>(this.serviceURL,task);
+  }
 
-   deleteTask(task:Task):Observable<Task>{
-    return this.http.delete<Task>(this.serviceUrl +'/' +task.id);
-   }
+  getAllTasks() : Observable<Task[]> {
+    return this.http.get<Task[]>(this.serviceURL);
+  }
 
-   editTask(task:Task):Observable<Task>{
-    return this.http.put<Task>(this.serviceUrl+'/'+task.id,task);
-   }
+  deleteTask(task : Task) : Observable<Task> {
+    return this.http.delete<Task>(this.serviceURL+'/'+task.id);
+  }
+
+  editTask(task : Task) : Observable<Task> {
+    return this.http.put<Task>(this.serviceURL+'/'+task.id,task);
+  }
+
 }
